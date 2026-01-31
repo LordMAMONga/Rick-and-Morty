43d47a00ccffd3840cdfbb271c4dc8c8a51a1609
LordMAMONga <amanmaster990@gmail.com>
2026-01-31 13:21:47 +0600

    hw-1

---
+++ Practic1/app/src/main/java/com/geeks/practic1/data/model/ProductDto.kt
@@ -0,0 +1,24 @@
+package com.geeks.practic1.data.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class ProductDto(
+    @SerialName("category")
+    val category: String?,
+    @SerialName("description")
+    val description: String?,
+    @SerialName("id")
+    val id: Int?,
+    @SerialName("image")
+    val image: String?,
+    @SerialName("price")
+    val price: Double?,
+    @SerialName("rating")
+    val rating: Rating?,
+    @SerialName("title")
+    val title: String?
+)
